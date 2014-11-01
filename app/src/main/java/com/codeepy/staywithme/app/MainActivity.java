5b607b37b96070cf38660be0df390876f5f9771b
mbaxkhb3 <cipherhat@gmail.com>
2014-11-01 18:04:03 +0000

    dommy

--- app/src/main/java/com/codeepy/staywithme/app/MainActivity.java
+++ app/src/main/java/com/codeepy/staywithme/app/MainActivity.java
@@ -1,29 +1,21 @@
 package com.codeepy.staywithme.app;
 
 import android.app.Activity;
-import android.app.PendingIntent;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.nfc.*;
-import android.nfc.tech.Ndef;
-import android.os.AsyncTask;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.UnsupportedEncodingException;
-import java.util.Arrays;
 
-
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     public static final String MIME_TEXT_PLAIN = "text/plain";
     public static final String TAG = "NfcDemo";
@@ -34,56 +26,66 @@ public class MainActivity extends Activity {
     private final int BOSS = 0;
     private final int MINION = 1;
     private int role;
+    private boolean isNfcExist = true;
+    private boolean isNfcEnabled;
+    private boolean isBluetoothEnabled;
+
+    private ImageButton btnNfc;
+    private ImageButton btnBluetooth;
+    private ImageButton btnSwm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnNfc = (ImageButton) findViewById(R.id.btn_nfc);
+        btnBluetooth = (ImageButton) findViewById(R.id.btn_bluetooth);
+        btnSwm = (ImageButton) findViewById(R.id.btn_swm);
+
+        btnNfc.setOnClickListener(this);
+    }
+
+    /**
+     * Called after {@link #onCreate} &mdash; or after {@link #onRestart} when
+     * the activity had been stopped, but is now again being displayed to the
+     * user.  It will be followed by {@link #onResume}.
+     * <p/>
+     * <p><em>Derived classes must call through to the super class's
+     * implementation of this method.  If they do not, an exception will be
+     * thrown.</em></p>
+     *
+     * @see #onCreate
+     * @see #onStop
+     * @see #onResume
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
         NfcManager manager = (NfcManager) getApplicationContext().getSystemService(Context.NFC_SERVICE);
         NfcAdapter adapter = manager.getDefaultAdapter();
         if (adapter == null) {
-            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+            isNfcExist = false;
             finish();
             return;
         }
 
         if (adapter.isEnabled()) {
-//            ((CheckBox)findViewById(R.id.checkbox_nfc)).setChecked(true);
-//            ((CheckBox)findViewById(R.id.checkbox_nfc)).setText("NFS is enabled");
-        }
-        else {
-//            ((CheckBox)findViewById(R.id.checkbox_nfc)).setChecked(false);
-//            ((CheckBox)findViewById(R.id.checkbox_nfc)).setText("NFS is disabled");
+            isNfcEnabled = true;
+            btnNfc.setBackground(getResources().getDrawable(R.drawable.button_on));
+        } else {
+            isNfcEnabled = false;
         }
 
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter.isEnabled()) {
-//            ((CheckBox)findViewById(R.id.checkbox_bluetooth)).setChecked(true);
-//            ((CheckBox)findViewById(R.id.checkbox_bluetooth)).setText("Bluetooth is enabled");
-        }
-        else {
-//            ((CheckBox)findViewById(R.id.checkbox_bluetooth)).setChecked(false);
-//            ((CheckBox)findViewById(R.id.checkbox_bluetooth)).setText("Bluetooth is disabled");
+            isBluetoothEnabled = true;
+            btnBluetooth.setBackground(getResources().getDrawable(R.drawable.button_on));
+        } else {
+            isBluetoothEnabled = false;
         }
 
-//        final Button button = (Button) findViewById(R.id.register);
-//        button.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                // Perform action on click
-//                role = MINION;
-//            }
-//        });
-//
-//        button.setOnLongClickListener(new View.OnLongClickListener() {
-//            public boolean onLongClick(View v) {
-//                // Perform action on click
-//                role = BOSS;
-//                return true;
-//            }
-//        });
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -102,5 +104,24 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_nfc:
+                if (!isNfcExist) {
+                    Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+                } else {
+                    if (!isNfcEnabled){
+                        startActivity(new Intent(android.provider.Settings.ACTION_NFC_SETTINGS));
+                    }
+                }
+        }
     }
 }
