226df169664e8c70a8d0fa9da733e37f6e00387f
dumbastic <dumbastic@gmail.com>
2014-11-01 15:53:19 +0000

    Update Bluetooth scanning

--- app/src/main/java/com/codeepy/staywithme/app/ScanBluetoothActivity.java
+++ app/src/main/java/com/codeepy/staywithme/app/ScanBluetoothActivity.java
@@ -13,12 +13,11 @@ import android.os.Bundle;
 import android.view.*;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
-//import com.codeepy.staywithme.app.factory.WebServiceFactory;
-//import com.codeepy.staywithme.app.factory.WebServicePostFactory;
-//import com.codeepy.staywithme.app.factory.WebServiceURLFactory;
-//import com.codeepy.staywithme.app.webservice.WebService;
-//import com.codeepy.staywithme.app.webservice.YoWebService;
-import android.widget.Toast;
+import com.codeepy.staywithme.app.factory.WebServiceFactory;
+import com.codeepy.staywithme.app.factory.WebServicePostFactory;
+import com.codeepy.staywithme.app.factory.WebServiceURLFactory;
+import com.codeepy.staywithme.app.webservice.WebService;
+import com.codeepy.staywithme.app.webservice.YoWebService;
 
 import java.util.ArrayList;
 
